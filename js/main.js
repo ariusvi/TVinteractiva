3a1f91d6b97daf85b3209a490010d0e4bd1c4e65
Ana Rius <a.riusvi11@gmail.com>
2024-02-04 23:49:55 +0100

    tras varios fallos y lios en el codigo, se cambia el formato más sencillo del diseño y del código

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//canal-botones
 const buttons = document.getElementsByClassName("button")
 
 let arrayButtons = Array.from(buttons)
@@ -9,7 +9,15 @@ arrayButtons.map(
     item => {
         item.addEventListener("click", (evento) => {
             screen.classList.remove(screen.classList[screen.classList.length - 1])
-            screen.classList.add("canal" + evento.target.id.slice(-1))
+            if(evento.target.id === "power"){
+                screen.classList.add("basicScreen")
+            } else {
+                screen.classList.add("canal" + evento.target.id.slice(-1))
+            }
+            
         })
     }
 )
+
+//boton power
+
