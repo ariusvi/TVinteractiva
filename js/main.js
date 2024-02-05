fd07c0f09bcc9bcb32d38e11a03d82147b417df0
Ana Rius <a.riusvi11@gmail.com>
2024-02-05 11:36:38 +0100

    rediseño del mando, cambio de imagenes para los canales, implementacion de boton de apagado (habria que revisar hacer que enciende y apague) e implementación de pantalla en el mando en la que se visualiza la fecha y hora

--- js/main.js
+++ js/main.js
@@ -1,23 +1,118 @@
-//canal-botones
-const buttons = document.getElementsByClassName("button")
 
-let arrayButtons = Array.from(buttons)
 
-let screen = document.getElementById("screen")
+let screen = document.getElementById("screen") 
 
-arrayButtons.map(
-    item => {
-        item.addEventListener("click", (evento) => {
-            screen.classList.remove(screen.classList[screen.classList.length - 1])
-            if(evento.target.id === "power"){
-                screen.classList.add("basicScreen")
-            } else {
-                screen.classList.add("canal" + evento.target.id.slice(-1))
-            }
-            
-        })
+setInterval(hora, 1000)
+
+function hora() {
+const date = new Date().toLocaleDateString()
+let hora = new Date().getHours()
+let minutos = new Date().getMinutes()
+let segundos = new Date().getSeconds()
+const time = `${hora}:${minutos < 10 ? "0" : ""}${minutos}:${
+    segundos < 10 ? "0" : ""
+}${segundos}`
+document.getElementById("hora").innerHTML = time
+document.getElementById("fecha").innerHTML = date
+}
+
+const screenHeader = document.getElementById("screenHeader")
+
+const power = document.getElementById("power")
+
+power.addEventListener("click", (evento) => {
+console.log(screen.classList)
+console.log(screen.classList.length)
+if (powerOn) {
+    console.log("encendido")
+    screen.classList = []
+}
+if (!powerOn) {
+    screen.classList = []
+    screen.classList.add("powerOff")
+}
+})
+
+let powerOn = false
+function toggle() {
+let screenHeader = document.getElementById("screenHeader")
+powerOn = !powerOn
+screenHeader.hidden = !screenHeader.hidden
+console.log(powerOn)
+}
+
+if (screenHeader.hidden) {
+console.log("apagado")
+} else {
+console.log("encendido")
+}
+
+buttonNumbers()
+
+function buttonNumbers() {
+const numbers = document.getElementsByClassName("numbers")
+
+let arrayNumbers = Array.from(numbers)
+
+screen = document.getElementById("screen") 
+
+arrayNumbers.map((item) => {
+    item.addEventListener("click", (evento) => {
+    console.log("presionaste un nro")
+    screen.classList.remove(screen.classList[screen.classList.length - 1])
+    console.log(screen.classList)
+    console.log(evento.target.innerHTML)
+    screen.classList.add("channel" + evento.target.innerHTML)
+
+    console.log(screen.classList)
+    })
+})
+}
+
+
+
+
+
+let channel
+let contChannel = 0
+channelButtons()
+function channelButtons() {
+const channelButton = document.getElementsByClassName("channel")
+console.log(channelButton[0])
+channelButton[0].addEventListener("click", (evento) => {
+    evento.target.innerHTML === "P +"
+    ? 
+        (channel(),
+        contChannel++,
+        screen.classList.add(`channel${contChannel}`),
+        (document.getElementById(
+        "channelInfo"
+        ).innerHTML = `Channel ${contChannel}`))
+    : 
+        (channel(),
+        contChannel--,
+        contChannel < 0
+        ? (console.log("es canal 0"),
+            console.log(screen.classList),
+            (contChannel = 9),
+            (document.getElementById(
+            "channelInfo"
+            ).innerHTML = `channel ${contChannel}`),
+            screen.classList.add(`Channel${contChannel}`))
+        : screen.classList.add(`Channel${contChannel}`),
+        (document.getElementById(
+        "channelInfo"
+        ).innerHTML = `channel ${contChannel}`))
+
+    function channel() {
+    console.log(screen.classList[0]),
+        (contChannel = screen.classList[0].substring(
+        screen.classList[0].length - 1,
+        screen.classList[0].length
+        )),
+        (screen.classList = [])
+    {
     }
-)
-
-//boton power
-
+    }
+})
+}
